f210b44fdbb59a2f2b22aa27a997508456020f6d
Maxat Akbanov <30049303+Brain2life@users.noreply.github.com>
2024-09-25 15:13:19 +0500

    Add go

---
+++ go/8kyu/convert-boolean-for-strings/convert_booleans_for_strings.go
@@ -0,0 +1,9 @@
+// Codewars link: https://www.codewars.com/kata/53369039d7ab3ac506000467/train/go
+package kata
+
+func BoolToWord(word bool) string {
+	if word {
+		return "Yes"
+	}
+	return "No"
+}
